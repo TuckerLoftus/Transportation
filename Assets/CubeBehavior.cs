515983755f448b6ad7ffc562f3ccbecf12f67e5c
TuckerLoftus <tuckerloftus97@gmail.com>
2015-10-19 14:01:03 -0400

    Transportation Part One
    part one

---
+++ Assets/CubeBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehavior : MonoBehaviour {
+
+	public Material WhiteColor ; 
+	public Material RedColor ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnMouseDown () {
+		for (int x = 0; x< MasterScript.AllCubes.Length; x++) {
+			MasterScript.AllCubes [x].GetComponent<CubeBehavior> ().turnWhite() ; 
+		}
+		turnRed (); 
+
+	}
+
+	void turnRed () {
+		GetComponent<Renderer> ().material = RedColor;
+	}
+
+	public void turnWhite () {
+		GetComponent<Renderer> ().material = WhiteColor; 
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/MasterScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterScript : MonoBehaviour {
+
+
+	public GameObject Cube ;
+	public static GameObject [] AllCubes = new GameObject[16] ; 
+
+	void SpawnCube (float x) {
+		AllCubes[(int)x]  = (GameObject) Instantiate (Cube, new Vector3 ((x * 2), 0), Quaternion.identity);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		for (int x = 0; x < 16; x++) {
+			SpawnCube (x);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
